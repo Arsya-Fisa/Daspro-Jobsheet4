b690ac411e43ff9cc65c7d592ba60d1bc743ed29
Arsya-Fisa <Arsya Fikri S>
2024-09-27 16:38:16 +0700

    commit ku

--- PemilihanBilangan01.java
+++ PemilihanBilangan01.java
@@ -4,19 +4,17 @@ public class PemilihanBilangan01 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int angka;
+        String hasil;
 
-        System.out.println("Masukan sebuah angka: ");
-        int angka = sc.nextInt();
-         if (angka % 2 == 0)
-         {
-            System.out.println("Angka" + angka + "Termasuk bilangan genap");
+        System.out.print("Masukan sebuah angka: ");
+        angka = sc.nextInt();
+         
+        hasil = (angka % 2 == 0) ? " termasuk bilangan positif" : "termasuk bilangan negatif";
+        
+        
+           System.out.println(angka + " adalah  " + hasil);
 
-         }
-         else
-         {
-            System.out.println("Angka" + angka + "Termasuk bilangan ganjil");
-
-         }
-
+        
 }
 }
---
+++ PemilihanHari01.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PemilihanHari01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String number, dayType;
+
+        System.out.println("Input Number: ");
+        number = sc.nextLine();
+
+        if () {
+            
+        }
+
+    }
+
+
+}
